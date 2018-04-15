e233ee356c55f65a21c525b6c2c917b3dc91eead
Takuya Noguchi <takuya@netlab.jp>
2018-04-15 18:54:54 +0900

    D - AtCoder社の冬(満点解法)

--- beginner/003/d/answer.rb
+++ beginner/003/d/answer.rb
@@ -1,3 +1,5 @@
+# Submission #2252452 を元に実装
+
 R, C = gets.split.map(&:to_i)
 X, Y = gets.split.map(&:to_i)
 D, L = gets.split.map(&:to_i)
@@ -7,7 +9,22 @@ def fact(n)
   (1..n).inject(:*)
 end
 
+def comb(n, r)
+  return 0 if n < 0 || r < 0 || n < r
+  fact(n) / (fact(r) * fact(n - r))
+end
+
+def pattern(x, y)
+  return 0 if x < 0 || y < 0
+  comb(x * y, D) * comb(x * y - D, L)
+end
+
 space_pattern = (R - X + 1) * (C - Y + 1)
-put_pattern = fact(X * Y) / (fact(D) * fact(L))
+put_pattern =  pattern(X, Y) -
+               pattern(X - 1, Y) * 2 - pattern(X, Y - 1) * 2 +
+               pattern(X - 2, Y) + pattern(X, Y - 2) +
+               pattern(X - 1, Y - 1) * 4 -
+               pattern(X - 1, Y - 2) * 2 - pattern(X - 2, Y - 1) * 2 +
+               pattern(X - 2, Y - 2)
 
 puts space_pattern * put_pattern % (10**9 + 7)
