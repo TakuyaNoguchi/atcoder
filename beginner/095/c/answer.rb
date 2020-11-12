6766ca5a3f4506937d46cd507b52e2a35c72f962
Takuya Noguchi <takuya@netlab.jp>
2020-11-12 21:55:44 +0900

    C - Half and Half

--- beginner/095/c/answer.rb
+++ beginner/095/c/answer.rb
@@ -1,29 +1,10 @@
 A, B, C, X, Y = gets.split.map(&:to_i)
 
-if X == Y
-  if C * 2 <= A || C * 2 <= B
-    puts C * 2 * X
-  else
-    puts A * X + B * Y
-  end
-
-  exit
-end
-
-if C * 2 <= A && C * 2 <= B
-  puts C * 2 * [X, Y].max
-elsif C * 2 <= A
-  diff = Y - X
-
-  puts C * 2 * X + B * (diff.positive? ? diff : 0)
-elsif C * 2 <= B
-  diff = X - Y
-
-  puts C * 2 * Y + A * (diff.positive? ? diff : 0)
-elsif C * 2 <= A + B
-  remain = (X < Y ? B : A) * (X - Y).abs
-
-  puts C * 2 * [X, Y].min + remain
-else
-  puts A * X + B * Y
-end
+puts 0.upto([X, Y].max).map { |ab|
+  # ABピザを2枚購入することで、1枚のAピザ、1枚のBピザを用意できる。
+  # つまりABピザを2枚買う場合、X - 1枚のAピザを購入、Y - 1枚のBピザを購入する
+  # ことで必要な枚数を用意することが出来る。
+  # よってABピザの枚数を総当たりし、そのABピザを購入した場合に要求される条件を満たすための
+  # Aピザ、Bピザの枚数を求め、Aピザ・Bピザ・ABピザの合計金額が最小となるものを解答とする。
+  C * 2 * ab + A * [0, X - ab].max + B * [0, Y - ab].max
+}.min
