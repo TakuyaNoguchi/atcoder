6e688a638ff4eb8b8dd51bcb016cd7581e555880
Takuya Noguchi <takuya@netlab.jp>
2021-12-14 19:34:24 +0900

    A - Cookie Exchanges

---
+++ boot_camp/easy/013/a/answer.rb
@@ -0,0 +1,14 @@
+a, b, c = gets.split.map(&:to_i)
+ans = 0
+
+while [a, b, c].all?(&:even?)
+    if a == b && b == c
+      ans = -1
+      break
+    end
+
+    a, b, c = b / 2 + c / 2, a / 2 + c / 2, a / 2 + b / 2
+    ans += 1
+end
+
+puts ans
